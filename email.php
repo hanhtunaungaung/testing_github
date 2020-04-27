595d7841d695598b3d7ed391ed18febb1e278ae6
HanHtunAung <46619661+hanhtunaungaung@users.noreply.github.com>
2020-04-27 21:46:02 +0630

    Update email.php

--- email.php
+++ email.php
@@ -2,15 +2,6 @@
 echo "Sending Email";
 echo "another email sent";
 
-echo "third line from cli";
-
-echo "three line on oline";
-echo "forth line from online";
-
-
-echo "fith line from online";
-
-echo "fifth line from cli";
 
 
 
